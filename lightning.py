bc3d2d707a495118a97abc64d9fa8902d18ee1d1
Matthew Davidson <mdavidson390@outlook.com>
2021-09-26 02:09:30 -0500

    BFS now works in discrete steps

--- lightning.py
+++ lightning.py
@@ -1,12 +1,13 @@
 import itertools as it
 from collections import deque
+from search import BreadthFirstSearch
 from time import sleep
 from typing import Generator, Union
 
 import graphics as g
 
 from cell import Cell
-from constants import BG_COLOR, COLS, FG_COLOR, HEIGHT, ROWS, WIDTH
+from constants import BG_COLOR, COLS, FG_COLOR, HEIGHT, REFRESH_RATE, ROWS, WIDTH
 from grid import Grid
 
 
@@ -35,34 +36,34 @@ def draw_gridlines(window: g.GraphWin, grid: Grid):
 	window.redraw()
 
 
-def bfs(grid: Grid, window: g.GraphWin, start: Cell) -> Union[Cell, None]:
-	q = deque([start])
-	visited = set()
+# def bfs(grid: Grid, window: g.GraphWin, start: Cell) -> Union[Cell, None]:
+# 	q = deque([start])
+# 	visited = set()
 
-	# while there are elements in the queue
-	while len(q) > 0:
-		curr = q.popleft()	# get first element from queue
+# 	# while there are elements in the queue
+# 	while len(q) > 0:
+# 		curr = q.popleft()	# get first element from queue
 
-		if curr in visited: continue
+# 		if curr in visited: continue
 
-		curr.sethighlight(Cell.HL_HIGH, window)
-		sleep(0.005)
+# 		curr.sethighlight(Cell.HL_HIGH, window)
+# 		sleep(0.005)
 		
-		# get its neighbors
-		adj = curr.getadjacent(grid)
+# 		# get its neighbors
+# 		adj = curr.getadjacent(grid)
 
-		# check if adjacent cells have been visited or are at end
-		for cell in adj:
-			if cell in visited:
-				adj.remove(cell)
-				continue
-			if cell.row == ROWS - 1:
-				return cell
+# 		# check if adjacent cells have been visited or are at end
+# 		for cell in adj:
+# 			if cell in visited:
+# 				adj.remove(cell)
+# 				continue
+# 			if cell.row == ROWS - 1:
+# 				return cell
 
-		visited.add(curr)	# current cell has been visited
-		q.extend(adj)		# add adjacent cells to queue
+# 		visited.add(curr)	# current cell has been visited
+# 		q.extend(adj)		# add adjacent cells to queue
 
-	return None
+# 	return None
 
 
 def update_highlights(visited: set[Cell], window: g.GraphWin):
@@ -71,31 +72,27 @@ def update_highlights(visited: set[Cell], window: g.GraphWin):
 			cell.sethighlight(cell.highlight - 1, window)
 
 
-def manhattan_dist(cell_1: Cell, cell_2: Cell) -> int:
-	return abs(cell_1.row - cell_2.row) + abs(cell_1.col - cell_2.col)
+# def dfs(grid: Grid, window: g.GraphWin, start: Cell, visited: set[Cell]) -> Union[Cell, None]:
+# 	if start.row == ROWS - 1:
+# 		return start
 
+# 	if start in visited: return None
 
-def dfs(grid: Grid, window: g.GraphWin, start: Cell, visited: set[Cell]) -> Union[Cell, None]:
-	if start.row == ROWS - 1:
-		return start
+# 	visited.add(start)
+# 	start.sethighlight(Cell.HL_HIGH, window)
+# 	sleep(0.05)
 
-	if start in visited: return None
+# 	adj = start.getadjacent(grid)
 
-	visited.add(start)
-	start.sethighlight(Cell.HL_HIGH, window)
-	sleep(0.05)
+# 	for cell in adj:
+# 		res = dfs(grid, window, cell, visited)
+# 		if res is None: continue
+# 		return res
 
-	adj = start.getadjacent(grid)
-
-	for cell in adj:
-		res = dfs(grid, window, cell, visited)
-		if res is None: continue
-		return res
-
-	return None
+# 	return None
 
 def main():
-	window = g.GraphWin('Graphics test', WIDTH, HEIGHT)
+	window = g.GraphWin('Lightning', WIDTH, HEIGHT, autoflush=False)
 	window.setBackground(BG_COLOR)
 	window.toScreen(0, 0)
 
@@ -105,18 +102,25 @@ def main():
 
 	start = grid[0, COLS // 2]
 
-	sleep(1)
-	(bfs(grid, window, start) or start).sethighlight(Cell.HL_HIGH, window)
-	sleep(2)
-
-	for cell in grid:
-		cell: Cell
-		cell.sethighlight(Cell.HL_OFF)
+	bfs = BreadthFirstSearch(grid, start, window)
+	done = False
 
 	sleep(1)
-	(dfs(grid, window, start, set()) or start).sethighlight(Cell.HL_HIGH, window)
-	sleep(2)
 
+	while not done:
+		# update_highlights(bfs.visited, window)
+
+		front = bfs.next_front()
+
+		for cell in front:
+			cell.sethighlight(Cell.HL_HIGH, window)
+			
+			if cell.row == ROWS - 1:
+				done = True
+
+		g.update(REFRESH_RATE)
+		
+	sleep(1)
 	window.close()
 
 
